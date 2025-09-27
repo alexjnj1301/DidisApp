8baee41ddb94fe74594d805f867933228ea49cd1
alexikicommit <alexis.jeanjean@ynov.com>
2025-09-27 14:32:27 +0200

    WIP: feat(lesson): manage lesson display

---
+++ src/app/pages/lesson/lesson.component.ts
@@ -0,0 +1,134 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit, ViewChild } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonCard, IonCardContent, IonCardHeader,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonImg,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonModal,
+  IonSkeletonText,
+  IonThumbnail,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone'
+import { Flashcard } from '../../models/Flashcard'
+
+@Component({
+  selector: 'app-lesson',
+  templateUrl: './lesson.component.html',
+  styleUrls: ['./lesson.component.scss'],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonTitle,
+    IonContent,
+    IonSkeletonText,
+    IonLabel,
+    IonThumbnail,
+    IonItem,
+    IonListHeader,
+    IonList,
+    IonCard,
+    IonImg,
+    IonCardTitle,
+    IonModal,
+    IonButton,
+    IonIcon,
+    IonCardHeader,
+    IonCardContent
+  ],
+  standalone: true,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class LessonComponent  implements OnInit {
+  @ViewChild(IonModal) modal!: IonModal;
+
+  private route = inject(ActivatedRoute)
+  public id = this.route.snapshot.paramMap.get('id')
+  public loaded = false
+  public src = "https://didis-app-test.s3.eu-west-3.amazonaws.com/b44ab3b56a5409066d0ee4450f3bf53ff5d82025.jpg"
+  // public src = "https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg"
+  public title = 'Image du cours'
+  public description = 'Cliquer ici permet de visualiser l\'image en grand'
+  public message = 'This modal example uses triggers to automatically open a modal when the button is clicked.'
+  public name!: string
+  public isModalOpen = false
+  public cards: Flashcard[] | undefined
+  public bullets = '<span class="bullets">1</span>'
+
+  constructor() { }
+
+  public ngOnInit() {
+    this.implementFlashcards()
+    setTimeout(() => {
+      this.loaded = true
+    }, 1000)
+  }
+
+  public setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
+  public downloadImage() {
+    const link = document.createElement('a');
+    link.href = this.src;
+    link.download = 'image.jpg';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  public implementFlashcards() {
+    this.cards = [
+      {
+        id: 1,
+        question: 'What is Ionic en vrai le sang ?',
+        answer: 'Ionic is a popular framework for building cross-platform mobile applications using web technologies like HTML, CSS, and JavaScript.',
+        lessonId: 1
+      },
+      {
+        id: 2,
+        question: 'What are some key features of Ionic?',
+        answer: 'Some key features of Ionic include a rich library of pre-built UI components, support for multiple platforms (iOS, Android, web), and integration with popular frameworks like Angular, React, and Vue.',
+        lessonId: 1
+      },
+      {
+        id: 3,
+        question: 'How does Ionic handle navigation?',
+        answer: 'Ionic uses a powerful navigation system that allows developers to create complex navigation patterns using a stack-based approach, similar to native mobile apps.',
+        lessonId: 3
+      }
+    ]
+  }
+
+  flipped = new Set<number>();
+
+  isFlipped(id: number) {
+    return this.flipped.has(id);
+  }
+
+  toggleFlip(id: number) {
+    if (this.flipped.has(id)) this.flipped.delete(id);
+    else this.flipped.add(id);
+    // force l’UI à se mettre à jour si besoin (pas nécessaire en général)
+  }
+
+  editCard(card: Flashcard) {
+    
+  }
+
+  deleteCard(id: number) {
+    
+  }
+}
